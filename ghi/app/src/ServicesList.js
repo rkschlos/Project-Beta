4711a932c58ca4727b7b1f8924b52201ceb0102e
allisha rectspat <allisharect@gmail.com>
2022-05-11 15:30:14 -0700

    buttons done

--- ghi/app/src/ServicesList.js
+++ ghi/app/src/ServicesList.js
@@ -1,4 +1,58 @@
-function ServicesList(props) {
+import React from 'react';
+
+class ServicesList extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {
+        appointments: [],
+      };
+    }
+  
+    async componentDidMount() {
+      const url = 'http://localhost:8080/api/service/';
+  
+      const response = await fetch(url);
+  
+      if (response.ok) {
+        const data = await response.json();
+        console.log(data)
+        this.setState({ appointments: data.appointments });
+      }
+    }
+  
+    async handleCancel(id) {
+      const cancelUrl = `http://localhost:8080/api/service/${id}/`;
+      const fetchConfig = {
+        method: "delete",
+      };
+
+      const response = await fetch(cancelUrl, fetchConfig);
+      console.log(response)
+      if (response.ok) {
+        console.log("deleted");
+        window.location.reload();
+      }
+    };
+
+    async handleFinished(id) {
+        const finishedUrl = `http://localhost:8080/api/service/${id}/`;
+        const fetchConfig = {
+          method: "put",
+          body: JSON.stringify({finished: true}),
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        };
+  
+        const response = await fetch(finishedUrl, fetchConfig);
+        console.log(response)
+        if (response.ok) {
+          console.log("updated");
+          window.location.reload();
+        }
+      };
+
+render() {
     return (
         <table className="table table-warning table-hover">
           <thead>
@@ -9,18 +63,25 @@ function ServicesList(props) {
               <th>Technician</th>
               <th>Reason</th>
               <th>VIP</th>
+              <th></th>
             </tr>
           </thead>
           <tbody>
-            {props.appointments.map(appointment => {
+            {this.state.appointments.map(appointment => {
+                let finished = ''
+                if (appointment.finished === true) {
+                    finished = 'd-none'
+                }
               return (
-                <tr key={appointment.id}>
+                <tr className={finished} key={appointment.id}>
                   <td>{ appointment.owner }</td>
                   <td>{ appointment.vin }</td>
                   <td>{ appointment.date_time }</td>
                   <td>{ appointment.technician }</td>
                   <td>{ appointment.reason }</td>
                   <td>{ appointment.is_vip ? "Yes" : "No" } </td>
+                  <td><button onClick={()=>this.handleCancel(appointment.id)} to="">Cancel</button></td>
+                  <td><button onClick={()=>this.handleFinished(appointment.id)} to="">Finished</button></td>
                 </tr>
               );
             })}
@@ -28,6 +89,6 @@ function ServicesList(props) {
         </table>
       );
     }
-    
-  
-  export default ServicesList;
+}
+
+export default ServicesList;
--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import api_appointments_list, api_technicians, api_delete_technician
+from .views import api_appointments_list, api_show_appointment, api_technicians, api_delete_technician
 
 urlpatterns = [
     path("service/", api_appointments_list, name="api_appointments_list"),
     path("service/new", api_appointments_list, name="create_appointment"),
+    path("service/<int:pk>/", api_show_appointment, name="api_show_appointment"),
     path("technicians/new", api_technicians, name="api_create_technician"),
     path("technicians/", api_technicians, name="api_technicians_list"),
     path("technicians/<int:pk>/", api_delete_technician, name="api_delete_technician")
